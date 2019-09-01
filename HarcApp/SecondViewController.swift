0336901e6ac58101876ce978a339934bc0c51580
Marcin Ślusarek <mrvrota@gmail.com>
2019-09-01 21:02:28 +0200

    Initial Commit

---
+++ HarcApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HarcApp
+//
+//  Created by Marcin Slusarek on 01/09/2019.
+//  Copyright © 2019 Marcin Slusarek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
