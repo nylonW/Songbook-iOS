fbbbfa2dcb1c7e21d5c6f047e44a51e02d15186b
Marcin Ślusarek <mrvrota@gmail.com>
2019-09-05 22:08:22 +0200

    Set dark mode for favourites tab, change app name to Śpiewnik

--- HarcApp/Sources/FavouriteSongs/EmptyFavouritesViewController.swift
+++ HarcApp/Sources/FavouriteSongs/EmptyFavouritesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmotyFavouritesViewController: UIViewController {
+class EmptyFavouritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
